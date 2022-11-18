494dd04b888837e8c23f80cd377933dbfea3f082
Lorenzo3dv <115221002+Lorenzo3dv@users.noreply.github.com>
2022-11-18 16:04:55 +0000

    lezione3

---
+++ lezione3.py
@@ -0,0 +1,12 @@
+def sum_csv(file_name):
+    my_file = open (file_name, 'r')
+    valori = []
+    for line in my_file:
+        elements = line.split(',')
+        if elements[1] != 'Sales\n':
+            valori.append(float(elements[1]))
+        somma = sum(valori)
+    my_file.close()
+    return somma
+
+print (sum_csv('shampoo_sales.txt'))
