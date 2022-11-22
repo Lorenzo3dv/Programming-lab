de7d64c853458bad48d2fb151d6ffacc394d7037
Lorenzo3dv <115221002+Lorenzo3dv@users.noreply.github.com>
2022-11-22 19:45:36 +0000

    lezione 4

---
+++ lezione4.py
@@ -0,0 +1,13 @@
+class CSVFile():
+    def __init__(self, name):
+        self.name = name
+
+    def get_data(self):
+        my_file = open(self.name)
+        my_list = []
+        for line in my_file:
+            elements = line.split(',')
+            if elements[1] != 'Sales\n':
+                my_list.append(elements)
+        return my_list
+
